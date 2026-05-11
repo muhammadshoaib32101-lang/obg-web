de4c8d6bc39873131b13e9983546c382118faa22
muhammadshoaib32101-lang <muhammadshoaib32101@gmail.com>
2026-05-11 22:18:42 +0500

    Delete app directory

--- app/admindashboard/page.jsx
+++
@@ -1,6 +0,0 @@
-"use client";
-import AdminDashboard from '../Components/admindashboard';
-
-export default function AdminDashboardPage() {
-  return <AdminDashboard />;
-}
--- app/dashboard/page.jsx
+++
@@ -1,6 +0,0 @@
-"use client";
-import UserDashboard from '../Components/dashboard';
-
-export default function AdminDashboardPage() {
-  return <UserDashboard />;
-}
