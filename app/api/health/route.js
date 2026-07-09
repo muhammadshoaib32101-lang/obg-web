f62bd9f97add1fa3fe99da12e3771d30ae8f3404
muhammadshoaib32101-lang <muhammadshoaib32101@gmail.com>
2026-07-09 10:49:10 +0500

    Delete app directory

--- app/api/health/route.js
+++
@@ -1,9 +0,0 @@
-// app/api/health/route.js
-import { NextResponse } from 'next/server';
-
-export async function GET() {
-  return NextResponse.json({ 
-    status: 'OK', 
-    message: 'Server is running' 
-  });
-}
--- app/signup/page.jsx
+++
@@ -1,6 +0,0 @@
-"use client";
-import Signup from '../Components/Registration/signup';
-
-export default function SignupPage() {
-  return <Signup />;
-}
